9eae15886eb2ed6a079f55435d09df7202abb914
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2022-08-18 10:31:41 +0100

    Update static.js

--- website/frontend/templates/static/static.js
+++ website/frontend/templates/static/static.js
@@ -1,2 +1,8 @@
+var today = new Date();
+var todayDate = today.getDate() + '/'+ (today.getMonth() + 1) + '/' + today.getFullYear();
+var todayTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var date = todayDate + ' ' + todayTime;
+
 var msg = "Current URL: " + document.location.protocol + "//" + document.location.hostname;
-console.log(msg);
+console.log("[DEBUG] " + msg);
